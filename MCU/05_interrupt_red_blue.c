2965367bf949a8710b32f7528fa5fb962b193372
HisameOgasahara <hisamexinori@gmail.com>
2026-09-07 21:11:28 +0900

    Fix interrupt practice II to use verified Switch 1 ERS3 path

--- MCU/05_interrupt_red_blue.c
+++ MCU/05_interrupt_red_blue.c
@@ -11,15 +11,15 @@
 #define PORT10_OUTPUT         (*(volatile unsigned int *)(PORT10_BASE_ADDRESS))
 #define PORT10_OMR            (*(volatile unsigned int *)(PORT10_BASE_ADDRESS + 0x04))
 
-/* Switch 2: Shield D3 -> TC275 P02.1 */
-#define SW2_PC1               11
+/* Switch 1: Easy Module Shield D2 -> TC275 P02.0 */
+#define SW1_PC0               3
 
-/* Red LED: Shield D12 -> TC275 P10.1 */
+/* Red LED: Easy Module Shield D12 -> TC275 P10.1 */
 #define RED_PC1               11
 #define RED_PS1               1
 #define RED_PCL1              17
 
-/* Blue LED: Shield D13 -> TC275 P10.2 */
+/* Blue LED: Easy Module Shield D13 -> TC275 P10.2 */
 #define BLUE_PC2              19
 #define BLUE_PS2              2
 #define BLUE_PCL2             18
@@ -29,10 +29,11 @@
 #define SCU_EICR1             (*(volatile unsigned int *)(SCU_BASE_ADDRESS + 0x214))
 #define SCU_IGCR0             (*(volatile unsigned int *)(SCU_BASE_ADDRESS + 0x22C))
 
-#define EXIS0                 4
-#define FEN0                  8
-#define EIEN0                 11
-#define INP0                  12
+/* EICR1 upper half controls Input Channel 3 (ERS3 / ETL3). */
+#define EXIS1                 20
+#define FEN1                  24
+#define EIEN1                 27
+#define INP1                  28
 #define IGP0                  14
 
 /* Service Request Control */
@@ -48,22 +49,26 @@ IfxCpu_syncEvent cpuSyncEvent = 0;
 volatile unsigned int blink_enabled = 1U;
 
 /*
- * Interrupt control practice II (slide 161)
- * Each button press alternates between blink and pause.
+ * Interrupt control practice II (slide 161).
+ * The slide says to stop/restart blinking with a button interrupt.
+ * This file continues from practice I and therefore uses the verified
+ * Switch 1 path:
+ *   D2 -> P02.0 -> REQ6 -> ERS3/In32 -> ETL3 -> OGU0 -> CPU0.
  */
 __interrupt(0x0F) __vector_table(0)
 void ISR0(void)
 {
+    /* Each Switch 1 interrupt alternates blink RUN/STOP. */
     blink_enabled ^= 1U;
 }
 
 void init_LEDs(void)
 {
-    /* P10.1 red LED -> push-pull general-purpose output */
+    /* P10.1 red LED -> push-pull general-purpose output. */
     PORT10_IOCR0 &= ~((0x1FU) << RED_PC1);
     PORT10_IOCR0 |=  ((0x10U) << RED_PC1);
 
-    /* P10.2 blue LED -> push-pull general-purpose output */
+    /* P10.2 blue LED -> push-pull general-purpose output. */
     PORT10_IOCR0 &= ~((0x1FU) << BLUE_PC2);
     PORT10_IOCR0 |=  ((0x10U) << BLUE_PC2);
 
@@ -78,29 +83,36 @@ void toggle_red_blue_LEDs(void)
                   (0x1U << BLUE_PCL2) | (0x1U << BLUE_PS2));
 }
 
-void init_switch2(void)
+void init_switch(void)
 {
-    /* P02.1 -> general-purpose input with pull-up */
-    PORT2_IOCR0 &= ~((0x1FU) << SW2_PC1);
-    PORT2_IOCR0 |=  ((0x02U) << SW2_PC1);
+    /* P02.0 uses PC0 field in PORT2_IOCR0. */
+    PORT2_IOCR0 &= ~((0x1FU) << SW1_PC0);
+
+    /* Set P02.0 to general-purpose input with pull-up. */
+    PORT2_IOCR0 |=  ((0x02U) << SW1_PC0);
 }
 
-void init_ERU_switch2(void)
+void init_ERU(void)
 {
-    /* P02.1 -> REQ14 -> ERS2 input 1, therefore EXIS0 = 001B. */
-    SCU_EICR1 &= ~(0x7U << EXIS0);
-    SCU_EICR1 |=  (0x1U << EXIS0);
+    /*
+     * Verified board/MCU path used by the working 05_interrupt_blue.c:
+     * P02.0 -> REQ6 -> ERS3 input In32.
+     * ERS3 is Input Channel 3, mapped to EICR1 upper-half fields.
+     * In32 is input number 2, therefore EXIS1 = 010B.
+     */
+    SCU_EICR1 &= ~(0x7U << EXIS1);
+    SCU_EICR1 |=  (0x2U << EXIS1);
 
-    /* Button press produces a falling edge. */
-    SCU_EICR1 |=  (0x1U << FEN0);
+    /* Pull-up switch press: High -> Low, detect falling edge. */
+    SCU_EICR1 |=  (0x1U << FEN1);
 
-    /* Enable trigger event generation. */
-    SCU_EICR1 |=  (0x1U << EIEN0);
+    /* Enable trigger event generation for ETL3. */
+    SCU_EICR1 |=  (0x1U << EIEN1);
 
-    /* Route ETL2 trigger to OGU0. */
-    SCU_EICR1 &= ~(0x7U << INP0);
+    /* Route ETL3 trigger event to OGU0. */
+    SCU_EICR1 &= ~(0x7U << INP1);
 
-    /* OGU0: activate IOUT0 whenever trigger arrives. */
+    /* OGU0: activate IOUT0 whenever a trigger event arrives. */
     SCU_IGCR0 &= ~(0x3U << IGP0);
     SCU_IGCR0 |=  (0x1U << IGP0);
 
@@ -108,14 +120,17 @@ void init_ERU_switch2(void)
     SRC_SCU_ERU0 &= ~(0xFFU);
     SRC_SCU_ERU0 |=  (0x0FU);
 
-    /* Enable service request and route it to CPU0. */
+    /* Enable service request. */
     SRC_SCU_ERU0 |=  (0x1U << SRE);
+
+    /* Route service request to CPU0. */
     SRC_SCU_ERU0 &= ~(0x3U << TOS);
 }
 
 void delay(void)
 {
     volatile unsigned int cycle;
+
     for (cycle = 0U; cycle < 20000000U; cycle++)
     {
         /* Busy wait. Interrupts remain enabled during this delay. */
@@ -126,18 +141,23 @@ void core0_main(void)
 {
     IfxCpu_enableInterrupts();
 
+    /* !!WATCHDOG0 AND SAFETY WATCHDOG ARE DISABLED HERE!!
+     * Enable the watchdogs and service them periodically if it is required.
+     */
     IfxScuWdt_disableCpuWatchdog(IfxScuWdt_getCpuWatchdogPassword());
     IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
 
+    /* Wait for CPU sync event. */
     IfxCpu_emitEvent(&cpuSyncEvent);
     IfxCpu_waitEvent(&cpuSyncEvent, 1);
 
     init_LEDs();
-    init_switch2();
-    init_ERU_switch2();
+    init_switch();
+    init_ERU();
 
     while (1)
     {
+        /* Slide 161: blink both LEDs continuously while enabled. */
         if (blink_enabled != 0U)
         {
             toggle_red_blue_LEDs();
@@ -145,7 +165,7 @@ void core0_main(void)
         }
         else
         {
-            /* Pause: keep the current LED states unchanged until next interrupt. */
+            /* Interrupt has paused blinking; keep current LED states. */
         }
     }
 }
