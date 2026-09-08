4afc1aba0632c395596fc566594f269a5ab55807
HisameOgasahara <hisamexinori@gmail.com>
2026-09-08 15:48:48 +0900

    Fix PWM register comments to match TC27x behavior

--- MCU/07_pwm.c
+++ MCU/07_pwm.c
@@ -93,22 +93,22 @@ void init_GTM_TOM0_PWM(void)
 {
     // Password Access to unlock CPU0 WDT Control Register 0
     SCU_WDTCPU0CON0 = ((SCU_WDTCPU0CON0 ^ 0xFC) & ~(1 << LCK)) | (1 << ENDINIT);
-    while ((SCU_WDTCPU0CON0 & (1 << LCK)) != 0);  // Wait until to unlock CPU0 WDT Control Register 0
+    while ((SCU_WDTCPU0CON0 & (1 << LCK)) != 0);  // Wait until CPU0 WDT Control Register 0 is unlocked
 
     // Modify Access to clear the ENDINIT bit in CPU0 WDT Control Register 0
     SCU_WDTCPU0CON0 = ((SCU_WDTCPU0CON0 ^ 0xFC) | (1 << LCK)) & ~(1 << ENDINIT);
-    while ((SCU_WDTCPU0CON0 & (1 << LCK)) == 0);  // Wait until to clear the ENDINIT bit in CPU0 WDT Control Register 0
+    while ((SCU_WDTCPU0CON0 & (1 << LCK)) == 0);  // Wait until the modify access is completed
 
     // Enable GTM Module
     GTM_CLC &= ~(1 << DISR);
 
     // Password Access to unlock CPU0 WDT Control Register 0
     SCU_WDTCPU0CON0 = ((SCU_WDTCPU0CON0 ^ 0xFC) & ~(1 << LCK)) | (1 << ENDINIT);
-    while ((SCU_WDTCPU0CON0 & (1 << LCK)) != 0);  // Wait until to unlock CPU0 WDT Control Register 0
+    while ((SCU_WDTCPU0CON0 & (1 << LCK)) != 0);  // Wait until CPU0 WDT Control Register 0 is unlocked
 
     // Modify Access to set the ENDINIT bit in CPU0 WDT Control Register 0
     SCU_WDTCPU0CON0 = ((SCU_WDTCPU0CON0 ^ 0xFC) | (1 << LCK)) | (1 << ENDINIT);
-    while ((SCU_WDTCPU0CON0 & (1 << LCK)) == 0);  // Wait until to clear the ENDINIT bit in CPU0 WDT Control Register 0
+    while ((SCU_WDTCPU0CON0 & (1 << LCK)) == 0);  // Wait until CPU0 WDT Control Register 0 is locked again
 
     while ((GTM_CLC & (1 << DISS)) != 0);          // Wait until GTM module is enabled
 
@@ -126,9 +126,9 @@ void init_GTM_TOM0_PWM(void)
     GTM_TOM0_TGC0_FUPD_CTRL |= ((0x2) << RSTCN0_CH1);     // Reset CN0 of TOM0 channel 1 on force update
 
     /*
-     * TOM0 CH2 비활성화 후 설정
-     * ENDIS_CTRL2 = 1 : channel disable request
-     * OUTEN_CTRL2 = 1 : output disable request
+     * TOM0 CH1 enable/output enable 설정
+     * ENDIS_CTRL1 = 0b10 : channel enable request
+     * OUTEN_CTRL1 = 0b10 : output enable request
      */
     GTM_TOM0_TGC0_ENDIS_CTRL |= ((0x2) << ENDIS_CTRL1);
     GTM_TOM0_TGC0_OUTEN_CTRL |= ((0x2) << OUTEN_CTRL1);
@@ -136,7 +136,7 @@ void init_GTM_TOM0_PWM(void)
     /*
      * TOM0 CH1 Control 설정
      *
-     * CLK_SRC_SR = 0 : FXCLK0 사용
+     * CLK_SRC_SR = 1 : FXCLK1 사용
      * SL         = 1 : PWM active level high
      */
     GTM_TOM0_CH1_CTRL |= (1 << SL);                         // High signal level for duty cycle
