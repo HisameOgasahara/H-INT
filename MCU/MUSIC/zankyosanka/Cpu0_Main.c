40680f089cc0d3a7f2b1c02edaeb95761a173980
HisameOgasahara <hisamexinori@gmail.com>
2026-09-09 10:42:55 +0900

    Add Zankyo Zanka Cpu0 main

---
+++ MCU/MUSIC/zankyosanka/Cpu0_Main.c
@@ -0,0 +1,26 @@
+#include "Ifx_Types.h"
+#include "IfxCpu.h"
+#include "IfxScuWdt.h"
+
+IfxCpu_syncEvent cpuSyncEvent = 0;
+
+extern void zankyosanka_run(void);
+
+int core0_main(void)
+{
+    IfxCpu_enableInterrupts();
+
+    IfxScuWdt_disableCpuWatchdog(IfxScuWdt_getCpuWatchdogPassword());
+    IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
+
+    IfxCpu_emitEvent(&cpuSyncEvent);
+    IfxCpu_waitEvent(&cpuSyncEvent, 1);
+
+    zankyosanka_run();
+
+    while (1)
+    {
+    }
+
+    return 1;
+}
