3db138b90fa1500f074ec0df469e38f3351b4600
HisameOgasahara <hisamexinori@gmail.com>
2026-09-09 11:29:21 +0900

    Switch KICK BACK score to MIDI-tick melody events

--- MCU/MUSIC/KICKBACK/kickback_score.h
+++ MCU/MUSIC/KICKBACK/kickback_score.h
@@ -1,21 +1,19 @@
 #ifndef KICKBACK_SCORE_H
 #define KICKBACK_SCORE_H
 
-#include "Ifx_Types.h"
-
 typedef struct
 {
     unsigned short frequency_hz; /* 0 = rest */
-    unsigned char  ticks_16th;   /* duration in 1/16-note units */
+    unsigned short midi_ticks;   /* duration in MIDI ticks, PPQ=128 */
 } KickBackEvent;
 
-#define KB_REST 0U
-#define KB_D3   147U
+#define KICKBACK_REST               0U
+#define KICKBACK_BPM                150U
+#define KICKBACK_TICKS_PER_QUARTER  128U
 
 extern const KickBackEvent g_kickback_score[];
 extern const unsigned int g_kickback_score_event_count;
 
-/* Keep the existing player source compatible after moving score data to .c. */
 #define KICKBACK_SCORE_EVENT_COUNT (g_kickback_score_event_count)
 
 #endif /* KICKBACK_SCORE_H */
