7f20bc0d4435aa1d92be6508629e62c26676a6f0
HisameOgasahara <hisamexinori@gmail.com>
2026-09-09 09:40:20 +0900

    Add Pokemon RBY opening monophonic RTTTL excerpt

---
+++ MCU/MUSIC/POKEMON_RBY_OPENING/pokemon_rby_score.c
@@ -0,0 +1,19 @@
+#include "pokemon_rby_score.h"
+
+/*
+ * Source basis: RTTTL.com "Pokemon Opening (Red, Blue, Yellow)", 192 BPM.
+ * This is a compact monophonic excerpt selected for the single-buzzer TC275
+ * exercise rather than the full 775-note transcription.
+ */
+const char g_pokemon_rby_opening_rtttl[] =
+    "16g4,64p,16b4.,64p,16d5.,64p,16f#5,64p,8g4.,64g4,32p.,8g4.,64g4,8p,32p.,"
+    "16g4,64p,16g4,64p,8g4.,64g4,32p.,8g4.,64g4,32p.,8g4.,64g4,32p.,"
+    "16f4.,64a3,16f4.,64a3,16f4,64p,16f4.,64a3,16f4.,64p,16f#4,64p,"
+    "4g4,16g4,64g4,32d3,64d2,8b4,64g3,4d5.,16d5,64g3,32p.,"
+    "4c4,16c4,64c4,32c3,64d2,8f4,64f3,4f5,16f5,64f5,32f3.,"
+    "16e5,64f3,16d#5,64p,4d5.,16d5,16g3,4f4,16f4,64f4,32g3.,"
+    "16e4,64g3,16d#4,64p,4d4.,16d4,16g3,8c4.,64d3,8b3,32b3,64g3,"
+    "8c4,32c4,64p,4g4,16g4,64g4,32d3,64d2,8b4,64g3,4d5.,16d5,64g3,32p.,"
+    "4c4.,16c4,16f3,8c5.,64c3,8b4,32b4,64f3,8c5,32c5,64p,"
+    "4d5.,16d5,16g3,8f4.,64d3,8e4,32e4,64g3,8c4,32c4,64p,"
+    "4d4.,16d4,8g3.,64p,8b3,64p,8c4,64g3,8d4,64p,4g4";
