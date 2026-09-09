6c52eb7f47397da5e825ee7fb2f2b5dad53ca101
HisameOgasahara <hisamexinori@gmail.com>
2026-09-09 09:40:10 +0900

    Add Pokemon RBY opening score interface

---
+++ MCU/MUSIC/POKEMON_RBY_OPENING/pokemon_rby_score.h
@@ -0,0 +1,12 @@
+#ifndef POKEMON_RBY_SCORE_H
+#define POKEMON_RBY_SCORE_H
+
+/*
+ * Monophonic RTTTL note sequence for the Pokemon Red/Blue/Yellow opening.
+ * The playback module parses this text at runtime.
+ */
+extern const char g_pokemon_rby_opening_rtttl[];
+
+#define POKEMON_RBY_BPM 192U
+
+#endif /* POKEMON_RBY_SCORE_H */
